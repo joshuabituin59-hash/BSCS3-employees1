35b70b9b5dbc9e9a4c93428c6a18fad436be88d7
joshuabituin59-hash <joshuabituin59@gmail.com>
2026-02-26 10:07:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 //Use routes
-app.use('api',routes)
+app.use('/api',routes)
 
 app.listen(process.env.PORT,() => {
     console.log(`Server is running on port ${process.env.PORT}`);
+
 });
